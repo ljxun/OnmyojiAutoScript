c1106f8904bf29a0e183b6a35d8b84b292a0558c
ljxun <591692130@qq.com>
2026-01-01 18:44:37 +0800

    refactor(SwitchAccount): 优化登录账户模块的界面处理逻辑
    - 将原有的简单条件判断替换为循环处理机制
    - 添加了对隐私协议接受的处理逻辑
    - 改进了服务器选择界面的检测和交互方式
    - 优化了4399登录活动弹窗的处理流程
    - 统一了界面元素出现和点击的处理模式

--- tasks/Component/SwitchAccount/login_account.py
+++ tasks/Component/SwitchAccount/login_account.py
@@ -48,10 +48,21 @@ class LoginAccount(BaseTask, SwitchAccountAssets):
         """
         self.screenshot()
         self.O_SA_LOGIN_FORM_SVR_NAME.keyword = svrName
-        # 4399登录会遇到活动-点击叉号
-        if self.wait_until_appear_then_click(RestartAssets.I_LOGIN_CLOSE, wait_time=1):
-            logger.info('4399登录会遇到活动-点击叉号')
-        self.ui_click(self.C_SA_LOGIN_FORM_SWITCH_SVR_BTN, self.I_SA_CHECK_SELECT_SVR_1, 1.5)
+
+        while 1:
+            self.screenshot()
+            if self.appear(self.I_SA_CHECK_SELECT_SVR_1):
+                logger.info('已在 选择服务器 界面的标志物')
+                break
+            if self.appear_then_click(RestartAssets.I_LOGIN_CLOSE, interval=1):
+                logger.info('4399登录会遇到活动-点击叉号')
+                continue
+            if self.appear_then_click(self.I_ACCEPT_AGREEMENT, interval=1):
+                logger.info('接受隐私协议')
+                continue
+            if self.click(self.C_SA_LOGIN_FORM_SWITCH_SVR_BTN, interval=1):
+                continue
+
         # 展开底部角色列表,显示角色所属服务器
         self.screenshot()
         if self.appear(self.I_SA_CHECK_SELECT_SVR_1) and (not self.appear(self.I_SA_CHECK_SELECT_SVR_2)):
@@ -115,10 +126,19 @@ class LoginAccount(BaseTask, SwitchAccountAssets):
         @rtype:
         """
         logger.info(f"[角色] 开始寻找角色: [{characterName}]")
-        # 4399登录会遇到活动-点击叉号
-        if self.wait_until_appear_then_click(RestartAssets.I_LOGIN_CLOSE, wait_time=1):
-            logger.info('4399登录会遇到活动-点击叉号')
-        self.ui_click(self.C_SA_LOGIN_FORM_SWITCH_SVR_BTN, self.I_SA_CHECK_SELECT_SVR_1)
+        while 1:
+            self.screenshot()
+            if self.appear(self.I_SA_CHECK_SELECT_SVR_1):
+                logger.info('已在 选择服务器 界面的标志物')
+                break
+            if self.appear_then_click(RestartAssets.I_LOGIN_CLOSE, interval=1):
+                logger.info('4399登录会遇到活动-点击叉号')
+                continue
+            if self.appear_then_click(self.I_ACCEPT_AGREEMENT, interval=1):
+                logger.info('接受隐私协议')
+                continue
+            if self.click(self.C_SA_LOGIN_FORM_SWITCH_SVR_BTN, interval=1):
+                continue
         # 展开底部角色列表,显示角色所属服务器
         self.screenshot()
         while (not self.appear(self.I_SA_CHECK_SELECT_SVR_2)) and self.appear(self.I_SA_CHECK_SELECT_SVR_1):
