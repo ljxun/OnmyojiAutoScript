60c1507a6c5de60c15728f6af8c5561f5a8dcd7d
ljxun <591692130@qq.com>
2026-01-06 16:43:20 +0800

    refactor(GlobalGame): 移除未使用的红色返回按钮图像资源
    - 从 image.json 中删除 ui_back_red 图像配置项
    - 从 assets.py 中删除对应的 I_UI_BACK_RED 规则图像定义
    - 清理了相关的 ROI 区域和模板匹配参数配置

--- tasks/GlobalGame/assets.py
+++ tasks/GlobalGame/assets.py
@@ -48,8 +48,6 @@ class GlobalGameAssets:
 	# '获得奖励' 四个大字 
 	I_UI_REWARD = RuleImage(roi_front=(481,185,317,42), roi_back=(464,142,350,145), threshold=0.73, method="Template matching", path="./ui/ui_ui_reward.png", file="./tasks/GlobalGame/ui/ui_ui_reward.png")
 	# description 
-	I_UI_BACK_RED = RuleImage(roi_front=(1041,111,34,38), roi_back=(834,15,427,200), threshold=0.8, method="Template matching", path="./ui/ui_ui_back_red.png", file="./tasks/GlobalGame/ui/ui_ui_back_red.png")
-	# description 
 	I_UI_AWARD = RuleImage(roi_front=(577,499,100,100), roi_back=(530,408,199,220), threshold=0.8, method="Template matching", path="./ui/ui_ui_award.png", file="./tasks/GlobalGame/ui/ui_ui_award.png")
 	# 短一点的确认 
 	I_UI_CONFIRM_SAMLL = RuleImage(roi_front=(677,390,130,62), roi_back=(677,390,130,62), threshold=0.8, method="Template matching", path="./ui/ui_ui_confirm_samll.png", file="./tasks/GlobalGame/ui/ui_ui_confirm_samll.png")
